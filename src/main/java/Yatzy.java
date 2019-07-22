f1e4a2d93dc21589d5420ef4da27412ecfc1c78e
Andy Smith <andysmith836@gmail.com>
2019-07-22 22:51:23 +0100

    Remove duplication

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,107 +1,73 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class Yatzy {
 
     private int[] dice;
     private final int[] tallies;
 
-    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = d5;
-
-        tallies = countDiceValues();
+    public Yatzy(int... dice) {
+        this.dice = dice;
+        this.tallies = countDiceValues();
     }
 
     public int chance() {
-        int sum = 0;
-        for (int die : dice) {
-            sum += die;
-        }
-        return sum;
+        return stream(dice).sum();
     }
 
     public int ones() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 1) {
-                sum += 1;
-            }
-        }
-        return sum;
-
+        return sumDice(1);
     }
 
     public int twos() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 2) {
-                sum += 2;
-            }
-        }
-        return sum;
+        return sumDice(2);
     }
 
     public int threes() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 3) {
-                sum += 3;
-            }
-        }
-        return sum;
+        return sumDice(3);
     }
 
-
     public int fours() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 4) {
-                sum += 4;
-            }
-        }
-        return sum;
+        return sumDice(4);
     }
 
     public int fives() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 5) {
-                sum += 5;
-            }
-        }
-        return sum;
+        return sumDice(5);
     }
 
     public int sixes() {
-        int sum = 0;
-        for (int die : dice) {
-            if (die == 6) {
-                sum += 6;
-            }
-        }
-        return sum;
+        return sumDice(6);
+    }
+
+    private int sumDice(int i) {
+        return stream(dice).filter(die -> die == i).map(die -> i).sum();
     }
 
     public int twoPair() {
         int n = 0;
         int score = 0;
-        for (int i = 0; i < 6; i += 1)
+        for (int i = 0; i < 6; i += 1) {
             if (tallies[6 - i - 1] >= 2) {
                 n++;
                 score += (6 - i);
             }
-        if (n == 2)
+        }
+
+        if (n == 2) {
             return score * 2;
-        else
-            return 0;
+        }
+
+        return 0;
     }
 
     public int scorePair() {
-        for (int i = 0; i < 6; i++)
-            if (tallies[6 - i - 1] >= 2)
-                return (6 - i) * 2;
+        for (int i = 5; i >= 0; i--) {
+            if (tallies[i] >= 2) {
+                return (i + 1) * 2;
+            }
+        }
+
         return 0;
     }
 
@@ -113,29 +79,28 @@ public class Yatzy {
     }
 
     public int smallStraight() {
-        if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1)
-            return 15;
-        return 0;
+        for (int i = 0; i < 5; i++) {
+            if (tallies[i] != 1) {
+                return 0;
+            }
+        }
+
+        return 15;
     }
 
     public int largeStraight() {
-        if (tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-                && tallies[5] == 1)
-            return 20;
-        return 0;
+        for (int i = 1; i < 6; i++) {
+            if (tallies[i] != 1) {
+                return 0;
+            }
+        }
+
+        return 20;
     }
 
     public int fullHouse() {
         return scorePair() + threeOfAKind();
     }
-
 
     public int threeOfAKind() {
         for (int i = 0; i < 6; i++) {
@@ -147,22 +112,17 @@ public class Yatzy {
     }
 
     public int yatzy() {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die - 1]++;
         for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
+            if (tallies[i] == 5)
                 return 50;
         return 0;
     }
 
     private int[] countDiceValues() {
         int[] tallies = new int[6];
-        tallies[dice[0] - 1]++;
-        tallies[dice[1] - 1]++;
-        tallies[dice[2] - 1]++;
-        tallies[dice[3] - 1]++;
-        tallies[dice[4] - 1]++;
+        for (int die : dice) {
+            tallies[die - 1]++;
+        }
         return tallies;
     }
 }
