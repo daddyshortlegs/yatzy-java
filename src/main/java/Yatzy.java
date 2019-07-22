3711314e6ed4cc2d74662f092df045e404b7215f
Andy Smith <andysmith836@gmail.com>
2019-07-22 22:11:27 +0100

    Create some loops

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -13,60 +13,48 @@ public class Yatzy {
 
     public int chance() {
         int sum = 0;
-        sum += dice[0];
-        sum += dice[1];
-        sum += dice[2];
-        sum += dice[3];
-        sum += dice[4];
+        for (int die : dice) {
+            sum += die;
+        }
         return sum;
-    }
-
-    public int yatzy() {
-        int[] counts = new int[6];
-        for (int die : dice)
-            counts[die - 1]++;
-        for (int i = 0; i != 6; i++)
-            if (counts[i] == 5)
-                return 50;
-        return 0;
     }
 
     public int ones() {
         int sum = 0;
-        if (dice[0] == 1) sum++;
-        if (dice[1] == 1) sum++;
-        if (dice[2] == 1) sum++;
-        if (dice[3] == 1) sum++;
-        if (dice[4] == 1) sum++;
-
+        for (int die : dice) {
+            if (die == 1) {
+                sum += 1;
+            }
+        }
         return sum;
+
     }
 
     public int twos() {
         int sum = 0;
-        if (dice[0] == 2) sum += 2;
-        if (dice[1] == 2) sum += 2;
-        if (dice[2] == 2) sum += 2;
-        if (dice[3] == 2) sum += 2;
-        if (dice[4] == 2) sum += 2;
+        for (int die : dice) {
+            if (die == 2) {
+                sum += 2;
+            }
+        }
         return sum;
     }
 
     public int threes() {
         int sum = 0;
-        if (dice[0] == 3) sum += 3;
-        if (dice[1] == 3) sum += 3;
-        if (dice[2] == 3) sum += 3;
-        if (dice[3] == 3) sum += 3;
-        if (dice[4] == 3) sum += 3;
+        for (int die : dice) {
+            if (die == 3) {
+                sum += 3;
+            }
+        }
         return sum;
     }
 
 
     public int fours() {
         int sum = 0;
-        for (int i = 0; i < 5; i++) {
-            if (dice[i] == 4) {
+        for (int die : dice) {
+            if (die == 4) {
                 sum += 4;
             }
         }
@@ -94,30 +82,20 @@ public class Yatzy {
     }
 
     public int scorePair() {
-        int[] counts = new int[6];
-        counts[dice[0] - 1]++;
-        counts[dice[1] - 1]++;
-        counts[dice[2] - 1]++;
-        counts[dice[3] - 1]++;
-        counts[dice[4] - 1]++;
+        int[] tallies = countDiceValues();
         int at;
         for (at = 0; at != 6; at++)
-            if (counts[6 - at - 1] >= 2)
+            if (tallies[6 - at - 1] >= 2)
                 return (6 - at) * 2;
         return 0;
     }
 
     public int twoPair() {
-        int[] counts = new int[6];
-        counts[dice[0] - 1]++;
-        counts[dice[1] - 1]++;
-        counts[dice[2] - 1]++;
-        counts[dice[3] - 1]++;
-        counts[dice[4] - 1]++;
+        int[] tallies = countDiceValues();
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
-            if (counts[6 - i - 1] >= 2) {
+            if (tallies[6 - i - 1] >= 2) {
                 n++;
                 score += (6 - i);
             }
@@ -127,42 +105,24 @@ public class Yatzy {
             return 0;
     }
 
+    public int threeOfAKind() {
+        int[] tallies = countDiceValues();
+        for (int i = 0; i < 6; i++)
+            if (tallies[i] >= 3)
+                return (i + 1) * 3;
+        return 0;
+    }
+
     public int fourOfAKind() {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[dice[0] - 1]++;
-        tallies[dice[1] - 1]++;
-        tallies[dice[2] - 1]++;
-        tallies[dice[3] - 1]++;
-        tallies[dice[4] - 1]++;
+        int[] tallies = countDiceValues();
         for (int i = 0; i < 6; i++)
             if (tallies[i] >= 4)
                 return (i + 1) * 4;
         return 0;
     }
 
-    public int threeOfAKind() {
-        int[] t;
-        t = new int[6];
-        t[dice[0] - 1]++;
-        t[dice[1] - 1]++;
-        t[dice[2] - 1]++;
-        t[dice[3] - 1]++;
-        t[dice[4] - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i + 1) * 3;
-        return 0;
-    }
-
     public int smallStraight() {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[dice[0] - 1] += 1;
-        tallies[dice[1] - 1] += 1;
-        tallies[dice[2] - 1] += 1;
-        tallies[dice[3] - 1] += 1;
-        tallies[dice[4] - 1] += 1;
+        int[] tallies = countDiceValues();
         if (tallies[0] == 1 &&
                 tallies[1] == 1 &&
                 tallies[2] == 1 &&
@@ -173,13 +133,7 @@ public class Yatzy {
     }
 
     public int largeStraight() {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[dice[0] - 1] += 1;
-        tallies[dice[1] - 1] += 1;
-        tallies[dice[2] - 1] += 1;
-        tallies[dice[3] - 1] += 1;
-        tallies[dice[4] - 1] += 1;
+        int[] tallies = countDiceValues();
         if (tallies[1] == 1 &&
                 tallies[2] == 1 &&
                 tallies[3] == 1 &&
@@ -190,7 +144,6 @@ public class Yatzy {
     }
 
     public int fullHouse() {
-        int[] tallies;
         boolean _2 = false;
         int i;
         int _2_at = 0;
@@ -198,12 +151,7 @@ public class Yatzy {
         int _3_at = 0;
 
 
-        tallies = new int[6];
-        tallies[dice[0] - 1] += 1;
-        tallies[dice[1] - 1] += 1;
-        tallies[dice[2] - 1] += 1;
-        tallies[dice[3] - 1] += 1;
-        tallies[dice[4] - 1] += 1;
+        int[] tallies = countDiceValues();
 
         for (i = 0; i != 6; i += 1)
             if (tallies[i] == 2) {
@@ -221,6 +169,26 @@ public class Yatzy {
             return _2_at * 2 + _3_at * 3;
         else
             return 0;
+    }
+
+    public int yatzy() {
+        int[] counts = new int[6];
+        for (int die : dice)
+            counts[die - 1]++;
+        for (int i = 0; i != 6; i++)
+            if (counts[i] == 5)
+                return 50;
+        return 0;
+    }
+
+    private int[] countDiceValues() {
+        int[] tallies = new int[6];
+        tallies[dice[0] - 1]++;
+        tallies[dice[1] - 1]++;
+        tallies[dice[2] - 1]++;
+        tallies[dice[3] - 1]++;
+        tallies[dice[4] - 1]++;
+        return tallies;
     }
 }
 
