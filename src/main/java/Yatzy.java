193cb4533201e50f5ec1f16806a1bb3be3f3b97d
Andy Smith <andysmith836@gmail.com>
2019-07-22 21:56:42 +0100

    initial clean up

---
+++ src/main/java/Yatzy.java
@@ -0,0 +1,238 @@
+public class Yatzy {
+
+    private final int d1;
+    private final int d2;
+    private final int d3;
+    private final int d4;
+    private final int d5;
+    private int[] dice;
+
+    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
+        this.d1 = d1;
+        this.d2 = d2;
+        this.d3 = d3;
+        this.d4 = d4;
+        this.d5 = d5;
+        dice = new int[5];
+        dice[0] = d1;
+        dice[1] = d2;
+        dice[2] = d3;
+        dice[3] = d4;
+        dice[4] = d5;
+    }
+
+    public int chance() {
+        int sum = 0;
+        sum += d1;
+        sum += d2;
+        sum += d3;
+        sum += d4;
+        sum += d5;
+        return sum;
+    }
+
+    public int yatzy() {
+        int[] counts = new int[6];
+        for (int die : dice)
+            counts[die - 1]++;
+        for (int i = 0; i != 6; i++)
+            if (counts[i] == 5)
+                return 50;
+        return 0;
+    }
+
+    public int ones() {
+        int sum = 0;
+        if (d1 == 1) sum++;
+        if (d2 == 1) sum++;
+        if (d3 == 1) sum++;
+        if (d4 == 1) sum++;
+        if (d5 == 1) sum++;
+
+        return sum;
+    }
+
+    public int twos() {
+        int sum = 0;
+        if (d1 == 2) sum += 2;
+        if (d2 == 2) sum += 2;
+        if (d3 == 2) sum += 2;
+        if (d4 == 2) sum += 2;
+        if (d5 == 2) sum += 2;
+        return sum;
+    }
+
+    public int threes() {
+        int sum = 0;
+        if (d1 == 3) sum += 3;
+        if (d2 == 3) sum += 3;
+        if (d3 == 3) sum += 3;
+        if (d4 == 3) sum += 3;
+        if (d5 == 3) sum += 3;
+        return sum;
+    }
+
+
+    public int fours() {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            if (dice[i] == 4) {
+                sum += 4;
+            }
+        }
+        return sum;
+    }
+
+    public int fives() {
+        int sum = 0;
+        for (int die : dice) {
+            if (die == 5) {
+                sum += 5;
+            }
+        }
+        return sum;
+    }
+
+    public int sixes() {
+        int sum = 0;
+        for (int die : dice) {
+            if (die == 6) {
+                sum += 6;
+            }
+        }
+        return sum;
+    }
+
+    public int scorePair() {
+        int[] counts = new int[6];
+        counts[d1 - 1]++;
+        counts[d2 - 1]++;
+        counts[d3 - 1]++;
+        counts[d4 - 1]++;
+        counts[d5 - 1]++;
+        int at;
+        for (at = 0; at != 6; at++)
+            if (counts[6 - at - 1] >= 2)
+                return (6 - at) * 2;
+        return 0;
+    }
+
+    public int twoPair() {
+        int[] counts = new int[6];
+        counts[d1 - 1]++;
+        counts[d2 - 1]++;
+        counts[d3 - 1]++;
+        counts[d4 - 1]++;
+        counts[d5 - 1]++;
+        int n = 0;
+        int score = 0;
+        for (int i = 0; i < 6; i += 1)
+            if (counts[6 - i - 1] >= 2) {
+                n++;
+                score += (6 - i);
+            }
+        if (n == 2)
+            return score * 2;
+        else
+            return 0;
+    }
+
+    public int fourOfAKind() {
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1 - 1]++;
+        tallies[d2 - 1]++;
+        tallies[d3 - 1]++;
+        tallies[d4 - 1]++;
+        tallies[d5 - 1]++;
+        for (int i = 0; i < 6; i++)
+            if (tallies[i] >= 4)
+                return (i + 1) * 4;
+        return 0;
+    }
+
+    public int threeOfAKind() {
+        int[] t;
+        t = new int[6];
+        t[d1 - 1]++;
+        t[d2 - 1]++;
+        t[d3 - 1]++;
+        t[d4 - 1]++;
+        t[d5 - 1]++;
+        for (int i = 0; i < 6; i++)
+            if (t[i] >= 3)
+                return (i + 1) * 3;
+        return 0;
+    }
+
+    public int smallStraight() {
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1 - 1] += 1;
+        tallies[d2 - 1] += 1;
+        tallies[d3 - 1] += 1;
+        tallies[d4 - 1] += 1;
+        tallies[d5 - 1] += 1;
+        if (tallies[0] == 1 &&
+                tallies[1] == 1 &&
+                tallies[2] == 1 &&
+                tallies[3] == 1 &&
+                tallies[4] == 1)
+            return 15;
+        return 0;
+    }
+
+    public int largeStraight() {
+        int[] tallies;
+        tallies = new int[6];
+        tallies[d1 - 1] += 1;
+        tallies[d2 - 1] += 1;
+        tallies[d3 - 1] += 1;
+        tallies[d4 - 1] += 1;
+        tallies[d5 - 1] += 1;
+        if (tallies[1] == 1 &&
+                tallies[2] == 1 &&
+                tallies[3] == 1 &&
+                tallies[4] == 1
+                && tallies[5] == 1)
+            return 20;
+        return 0;
+    }
+
+    public int fullHouse() {
+        int[] tallies;
+        boolean _2 = false;
+        int i;
+        int _2_at = 0;
+        boolean _3 = false;
+        int _3_at = 0;
+
+
+        tallies = new int[6];
+        tallies[d1 - 1] += 1;
+        tallies[d2 - 1] += 1;
+        tallies[d3 - 1] += 1;
+        tallies[d4 - 1] += 1;
+        tallies[d5 - 1] += 1;
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 2) {
+                _2 = true;
+                _2_at = i + 1;
+            }
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 3) {
+                _3 = true;
+                _3_at = i + 1;
+            }
+
+        if (_2 && _3)
+            return _2_at * 2 + _3_at * 3;
+        else
+            return 0;
+    }
+}
+
+
+
